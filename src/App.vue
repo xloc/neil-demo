12c7a64a2038396a2ad5686af2aa3bfdb3bfcd5c
xloc <xloc.cc@outlook.com>
2024-01-03 01:11:44 -0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="p-5 border rounded-lg hover:shadow-lg">
+      <p><span class="font-bold">Name:</span> Neil Fan</p>
+    </div>
+  </div>
+</template>
